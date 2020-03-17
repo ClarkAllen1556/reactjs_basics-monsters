d88821855c165e2a02f86be2cd6c8a9a101ae386
Allen <allenclark@u.boisestate.edu>
2020-03-17 15:17:49 +0900

    Updates

---
+++ precache-manifest.5602ddb624f027a80214d9dd2defaf88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366cc65db1258052cd794eb2c4ed1856",
+    "url": "/reactjs_basics-monsters/index.html"
+  },
+  {
+    "revision": "546c20383d66f67d6f89",
+    "url": "/reactjs_basics-monsters/static/css/main.26ef0767.chunk.css"
+  },
+  {
+    "revision": "3e6c0387681a5887ae2f",
+    "url": "/reactjs_basics-monsters/static/js/2.f79cd886.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactjs_basics-monsters/static/js/2.f79cd886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546c20383d66f67d6f89",
+    "url": "/reactjs_basics-monsters/static/js/main.6189856e.chunk.js"
+  },
+  {
+    "revision": "909214e00897309517d8",
+    "url": "/reactjs_basics-monsters/static/js/runtime-main.029cc389.js"
+  }
+]);
